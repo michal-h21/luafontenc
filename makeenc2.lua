e49a9c6a807090bec355a0d575c17a3122982283
Michal Hoftich <michal.h21@gmail.com>
2015-05-27 15:52:14 +0200

    Generate needed format

--- makeenc2.lua
+++ makeenc2.lua
@@ -3,7 +3,8 @@ local enc = loadenc.load(arg[1])
 local symbols = loadenc.parse(enc)
 local glyphs = require "glyphload"
 
+print "local m = {" 
 for k,v in pairs(symbols) do 
-  print(k,v, glyphs:getGlyph(v))
+  print("["..k .."] = 0x" .. (glyphs:getGlyph(v) or "nil") ..",")
 end
-
+print "}"
